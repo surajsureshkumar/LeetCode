08a9f2931d77012a03fe6934cb4f5cd68ba82c0d
Suraj S <surajsureshkumar98@gmail.com>
2023-01-20 08:34:20 -0500

    Sum of Two Numbers

---
+++ sum_of_number.js
@@ -0,0 +1,4 @@
+//Javascript solution
+var sum = function(num1, num2) {
+    return num1 + num2
+};
--- sum_of_numbers2235.py
+++ sum_of_numbers2235.py
@@ -1,8 +1,3 @@
-# Javascript solution
-var sum = function(num1, num2) {
-    return num1 + num2
-};
-
 # Python Solution
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
